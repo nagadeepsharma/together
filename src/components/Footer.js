d82f618bc943c5aaa2f2f72358931e9323012fed
nagadeepsharma <nagadeep.chandragiri@gmail.com>
2021-06-04 12:25:02 +0530

    pics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Grid, GridColumn, GridRow, Header, Icon } from 'semantic-ui-react'
+import { Button, Grid, GridColumn, GridRow, Header, Icon, Modal,Image } from 'semantic-ui-react'
+
 import '../css/footer.css'
 
 function Footer() {
+    const [open, setOpen] = React.useState(false)
     return (
+        <>
         <div className="footer">
             <Grid className="gridfooter">
-            <GridRow>
+            <GridRow style={{marginTop:"1%"}}>
                 <GridColumn><Header  className="footerlinks" as="h3" textAlign="center">Contact</Header>
                 <GridRow style={{marginTop:"10px"}}>
                 <GridColumn>
                 <Header  className="footerlinks" as="h5" textAlign="center">(+91)9502542045<br/>nagadeep.chandragiri@gmail.com</Header>
                 </GridColumn>
                 </GridRow>
-
+                </GridColumn>
+                <GridColumn><Header  as="h3" className="footerlinks" textAlign="center">About</Header>
                 <GridRow style={{marginTop:"10px"}}>
                 <GridColumn style={{display:"flex",justifyContent:"space-between",width:"150px"}}>
                     <Icon className="i"  size="big" name='instagram' />
@@ -23,18 +27,48 @@ function Footer() {
                 </GridColumn>
                 </GridRow>
                 </GridColumn>
-                <GridColumn><Header  as="h3" className="footerlinks" textAlign="center">About</Header></GridColumn>
-                <GridColumn><Header  as="h3" className="footerlinks" textAlign="center">Team</Header></GridColumn>
+                <GridColumn><Header  as="h3" className="footerlinks" textAlign="center">
+                <Modal style={{width:"70%"}}
+                    onClose={() => setOpen(false)}
+                    onOpen={() => setOpen(true)}
+                    open={open}
+                    trigger={<Button primary style={{width:"180px"}}>Team</Button>}
+                >
+                <Modal.Header>Team</Modal.Header>
+        <div style={{display:"flex",justifyContent:"space-evenly"}}>
+         <Modal.Content  image>
+           <Image size="medium" src='https://react.semantic-ui.com/images/wireframe/image-square.png' wrapped />
+         </Modal.Content>
+         <Modal.Content image>
+           <Image size='medium' src='https://react.semantic-ui.com/images/wireframe/image-square.png' wrapped />
+         </Modal.Content>
+         <Modal.Content image>
+           <Image size='medium' src='https://react.semantic-ui.com/images/wireframe/image-square.png' wrapped />
+         </Modal.Content>
+         </div>
+         <Modal.Actions>
+           <Button onClick={() => setOpen(false)}>Cancel</Button>
+           <Button onClick={() => setOpen(false)} positive>
+             Ok
+           </Button>
+         </Modal.Actions>
+         </Modal>
+                </Header></GridColumn>
             </GridRow>
            
             <GridRow className="gridsocial">
             
             </GridRow>
             </Grid>
-            <div style={{height:"5vh",backgroundColor:"grey",display:"flex",justifyContent:"center",alignItems:"center"}}>
+            <div style={{height:"5vh",backgroundColor:"#333",display:"flex",justifyContent:"center",alignItems:"center"}}>
                 <Header as="h3" style={{color:"#f4f4f4",letterSpacing:"1px"}}>togEther &copy; 2021</Header>
             </div>
         </div>
+
+        
+         
+       
+       </>
     )
 }
 
